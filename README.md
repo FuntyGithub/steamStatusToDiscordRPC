fb19f3a45e0f73e7939ffa4755d2bb89708b4505
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2023-02-13 19:49:33 +0100

    gamma

---
+++ README.md
@@ -0,0 +1,15 @@
+# Steam status to Discord RPC
+This script get the Steam-status of a user and display it as discord rich presence.
+
+![](https://cdn.discordapp.com/attachments/1018129353328037948/1074763419787477192/steamtodiscord.png)
+
+## Installation
+
+1. Download the files.
+2. Run ``npm i`` to install the packages.
+3. Change the SteamID in the ``config.json``.
+4. Run ``node index.js`` to run the script.
+
+# Disclaimer
+If you use this script, you do it on your own risk!
+This script may violates the Terms of Service of any service. I don't take any responsibilities.
--- index.js
+++ index.js
@@ -2,74 +2,67 @@ const config = require('./config.json');
 const playwright = require('playwright');
 const DiscordRPC = require('discord-rpc');
 
-
 DiscordRPC.register(config.clientID);
-
 
 const rpc = new DiscordRPC.Client({ transport: 'ipc' });
 
 rpc.on('ready', () => {
 
     playwright.chromium.launch().then(async (browser) => {
-        console.log('Launching browser...');
-    
-        const page = await browser.newPage();
+        
+        let page = await browser.newPage();
+        
         let data = await getSteamPresence(page);
         if (data) {
             updateActivity(data);
         } else {
             console.log('User is not playing a game');
-            // rpc.setActivity({
-            //     details: 'User is not playing a game',
-            //     state: 'User is not playing a game',
-            //     largeImageKey: 'user',
-            //     instance: false,
-            //     buttons: [
-            //         { label: 'Steam', url: `https://steamcommunity.com/profiles/${config.steamID}/` },
-            //         { label: 'Game', url: `https://store.steampowered.com/app/` }
-            //     ]
-            // });
         }
-    
+
+        // start refresh interval
+        setInterval(async () => {
+            console.log('Checking for new game...');
+            let data = await getSteamPresence(page);
+            if (data) {
+                updateActivity(data);
+            } else {
+                console.log('User is not playing a game');
+                rpc.clearActivity();
+            }
+        }, config.refreshTime);
+
     });
     
 });
 
 rpc.login({ clientId: config.clientID }).catch(console.error);
 
-
-
 async function getSteamPresence(page) {
+    console.log('Getting user presence...');
 
     await page.goto(`https://steamcommunity.com/profiles/${config.steamID}/`);
-
 
     try {
         await page.waitForSelector('div.playerAvatar');
         await page.hover('div.playerAvatar');
         await page.waitForSelector('div.miniprofile_hover');
-        await page.screenshot({ path: 'save.png' });
-        // das hier drüber ist nur zum testen!!
     } catch (error) {
-        await page.screenshot({ path: 'saveERR.png' });
-        // das hier drüber ist nur zum testen!!
     }
 
-    // if div with class miniprofile_gamesection is found, then the user plays a game
+    // check if user is playing a game
     if (await page.$('div.miniprofile_gamesection')) {
 
         console.log('User is playing a game');
 
+        // get game info
         await page.waitForSelector('img.game_logo');
         let element = await page.$('div.miniprofile_gamesection');
         let logo = await element.$eval('img.game_logo', (el) => el.src);
-        // console.log(logo);
         let state = await element.$eval('span.game_state', (el) => el.innerText);
         let name = await element.$eval('span.miniprofile_game_name', (el) => el.innerText);
         let presence = await element.$eval('span.rich_presence', (el) => el.innerText);
         let gameID = logo.split('/')[5];
 
-        // console.log(gameID);
         return {
             logo: logo,
             state: state,
@@ -77,29 +70,25 @@ async function getSteamPresence(page) {
             presence: presence,
             gameID: gameID
         }
-
-        // const game = await page.$eval('div.miniprofile_gamesection', (el) => el.innerText);
-        // console.log(game);
-    } else {
-        console.log('User is not playing a game');
-        return false;
-        // console.log(await page.innerHTML('body')) 
-    }
+    } else return false;
 }
 
 function updateActivity(data) {
+    console.log('Updating activity...');
     let activity = {
-        details: data.state,
+        details: data.name,
         state: data.presence,
         largeImageKey: data.logo,
-        largeImageText: data.name,
+        largeImageText: data.state,
         instance: false,
         buttons: [
-            { label: 'Steam', url: `https://steamcommunity.com/profiles/${config.steamID}/` },
-            { label: 'Game', url: `https://store.steampowered.com/app/${data.gameID}/` }
+            { label: 'Steam User', url: `https://steamcommunity.com/profiles/${config.steamID}/` },
+            { label: 'Steam Game', url: `https://store.steampowered.com/app/${data.gameID}/` }
         ]
     }
-    // console.log(activity);
-    rpc.setActivity(activity);
-    
+
+    rpc.setActivity(activity).catch(error => {
+        if(error) console.log(error)
+        else console.log('Activity successfully updated!');
+    });
 }
